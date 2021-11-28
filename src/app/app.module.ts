aef9a9b1fa3e63cb2b69c4a393ab9d9c8155b039
Rachit_Jain <rachit.rj11@gmail.com>
2021-11-28 23:44:56 +0530

    TASK:2: Style the WebPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Part1Component } from './part1/part1.component';
+import { Part2Component } from './part2/part2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Part1Component
+    Part1Component,
+    Part2Component
   ],
   imports: [
     BrowserModule,
